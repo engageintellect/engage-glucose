e003f1e6ab1e5f2e6dc1f48bae0b8e8e46a9f0eb
engageintellect <jc9361@gmail.com>
2023-10-08 10:39:57 -0700

    init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
+	import { showEvents } from '$lib/stores';
 	import { onDestroy } from 'svelte';
 	export let data: any;
 
-	let showEvents = false;
-	let showAlarms = false;
+	// let showEvents = false;
+	let showAlarms = true;
 
 	function refreshPage() {
 		location.reload(); // Reloads the current page
@@ -21,14 +22,12 @@
 		clearInterval(intervalId);
 	});
 
-	function toggleAlarms() {
-		showAlarms = !showAlarms;
-	}
-
 	function toggleEvents() {
-		showEvents = !showEvents;
+		showEvents.update((n) => !n);
 	}
 </script>
+
+<!-- <div class="text-red-500">{$showEvents}</div> -->
 
 <div class="flex w-full h-full min-h-screen break-words">
 	<div
@@ -49,7 +48,7 @@
 				<!-- ################################################### -->
 
 				<div
-					class="flex justify-between gap-1 h-full w-full items-end text-teal-500 font-extrabold text-7xl md:text-7xl my-5"
+					class="flex justify-between gap-1 h-full w-full items-end text-teal-500 font-extrabold text-7xl md:text-7xl lg:text-8xl my-5"
 				>
 					<div class="flex gap-1 items-end">
 						<div>{data.glucoseValue}</div>
@@ -88,58 +87,8 @@
 			{/if}
 
 			<div class="w-full flex flex-col items-center justify-center gap-2">
-				<button
-					class="border border-teal-500 md:hover:border-teal-400 text-teal-500 md:hover:text-teal-400 transition-all duration-200 py-4 px-6 rounded w-full font-bold uppercase flex justify-center"
-					on:click={toggleAlarms}
-				>
-					{#if showAlarms}
-						<div class="flex gap-2 items-center">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke-width="1.5"
-								stroke="currentColor"
-								class="w-6 h-6"
-							>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
-								/>
-							</svg>
-
-							<div>Hide Alarms</div>
-						</div>
-					{:else}
-						<div class="flex gap-2 items-center">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke-width="1.5"
-								stroke="currentColor"
-								class="w-6 h-6"
-							>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-								/>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-								/>
-							</svg>
-
-							<div>Show Alarms</div>
-						</div>
-					{/if}
-				</button>
-
 				{#if showAlarms}
-					<div class="p-4 bg-neutral-800 rounded w-full">
+					<div class="p-4 bg-neutral-800 rounded w-full my-2">
 						<div class="text-xl md:text-2xl text-neutral-200">
 							<div class="flex gap-1 items-center">
 								<svg
@@ -172,7 +121,7 @@
 					class="border border-teal-500 md:hover:border-teal-400 text-teal-500 md:hover:text-teal-400 transition-all duration-200 py-4 px-6 rounded w-full font-bold uppercase flex justify-center"
 					on:click={toggleEvents}
 				>
-					{#if showEvents}
+					{#if $showEvents}
 						<div class="flex gap-2 items-center">
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
@@ -221,9 +170,9 @@
 				<!-- ################################################### -->
 				<!-- OUT OF BOUNDS EVENTS -->
 				<!-- ################################################### -->
-				{#if showEvents}
+				{#if $showEvents}
 					<div class="p-4 bg-neutral-800 rounded w-full">
-						<div class="mt-5 text-lg md:text-2xl text-neutral-200">
+						<div class="text-lg md:text-2xl text-neutral-200">
 							<div class="flex gap-2 items-center">
 								<svg
 									xmlns="http://www.w3.org/2000/svg"
@@ -242,18 +191,14 @@
 								<div class="font-semibold">Range Events</div>
 							</div>
 
-							<!-- <div class="text-3xl mb-2">Out of range events</div> -->
 							<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 w-full gap-2">
-								{#each data.graphData as graphData}
+								{#each data.graphData.reverse() as graphData}
 									{#if graphData.Value >= 120 || graphData.Value <= 70}
-										<div class="flex flex-col items-start bg-neutral-800 rounded">
+										<div class="flex flex-col items-start rounded">
 											<div class="">
 												<div class="text-red-500 font-semibold">
 													{graphData.Value} <span class="text-sm text-neutral-100">mg/dL</span>
 												</div>
-												<!-- {:else}  -->
-												<!-- <div class="text-emerald-500 font-semibold text-neutral-300">{graphData.Value}</div> -->
-												<!-- <div class="text-sm underline">mg/dL</div> -->
 											</div>
 											<div class="text-neutral-300 text-xs">{graphData.Timestamp}</div>
 										</div>
@@ -314,9 +259,9 @@
 				</a>
 			</div>
 
-			<div class="text-sm w-md break-word mt-10">
+			<div class="text-sm w-md break-word mt-10 flex justify-center">
 				<div>
-					Visit <a href="https://github.com/engaeintellect/engage-glucose"
+					Visit <a href="https://github.com/engageintellect/engage-glucose"
 						><span class="underline">repository</span></a
 					> to read the documentation
 				</div>
