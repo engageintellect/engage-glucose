4589af18544e112209b8b644cf2f18082d81a92c
engageintellect <jc9361@gmail.com>
2023-10-08 09:15:25 -0700

    init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	export let data: any;
 
 	let showEvents = false;
+	let showAlarms = false;
 
 	function refreshPage() {
 		location.reload(); // Reloads the current page
@@ -19,6 +20,10 @@
 	onDestroy(() => {
 		clearInterval(intervalId);
 	});
+
+	function toggleAlarms() {
+		showAlarms = !showAlarms;
+	}
 
 	function toggleEvents() {
 		showEvents = !showEvents;
@@ -39,11 +44,17 @@
 					{data.patient.data[0].glucoseMeasurement.Timestamp}
 				</div>
 
+				<!-- ################################################### -->
+				<!-- CURRENT GLUCOSE -->
+				<!-- ################################################### -->
+
 				<div
-					class="flex gap-1 h-full w-full items-end text-teal-500 font-extrabold text-7xl md:text-7xl my-5"
+					class="flex justify-between gap-1 h-full w-full items-end text-teal-500 font-extrabold text-7xl md:text-7xl my-5"
 				>
-					<div>{data.glucoseValue}</div>
-					<div class="text-3xl">mg/dL</div>
+					<div class="flex gap-1 items-end">
+						<div>{data.glucoseValue}</div>
+						<div class="text-2xl">mg/dL</div>
+					</div>
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						fill="none"
@@ -61,35 +72,27 @@
 				</div>
 			</div>
 
-			<div class="text-2xl sm:text-3xl font-semibold text-neutral-300">Blood Glucose Normal.</div>
-			<div class="p-4 bg-neutral-800 rounded mt-2">
-				<div class="text-xl md:text-2xl text-neutral-200">
-					<div class="flex gap-2 items-center">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							class="w-6 h-6"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
-							/>
-						</svg>
-						<div class="font-semibold">Alarms</div>
-					</div>
+			<!-- ################################################### -->
+			<!-- CURRENT STATUS -->
+			<!-- ################################################### -->
 
-					<div class="flex gap-5 text-lg md:text-xl font-thin">
-						<div class="">High: {data.patient.data[0].targetHigh}</div>
-						<div class="">Low: {data.patient.data[0].targetLow}</div>
-					</div>
-				</div>
+			{#if !data.patient.data[0].glucoseMeasurement.isLow || !data.patient.data[0].glucoseMeasurement.isHigh}
+				<div class="text-2xl sm:text-3xl font-semibold text-neutral-300">Blood Glucose Normal.</div>
+			{:else}
+				{#if data.patient.data[0].glucoseMeasurement.isLow}
+					<div class="text-2xl sm:text-3xl font-semibold text-yellow-500">Glucose Low!</div>
+				{/if}
+				{#if !data.patient.data[0].glucoseMeasurement.isHigh}
+					<div class="text-2xl sm:text-3xl font-semibold text-red-500">Glucose High!</div>
+				{/if}
+			{/if}
 
-				{#if showEvents}
-					<div class="mt-5 text-lg md:text-2xl text-neutral-200">
+			<div class="w-full flex flex-col items-center justify-center gap-2">
+				<button
+					class="border border-teal-500 md:hover:border-teal-400 text-teal-500 md:hover:text-teal-400 transition-all duration-200 py-4 px-6 rounded w-full font-bold uppercase flex justify-center"
+					on:click={toggleAlarms}
+				>
+					{#if showAlarms}
 						<div class="flex gap-2 items-center">
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
@@ -102,35 +105,69 @@
 								<path
 									stroke-linecap="round"
 									stroke-linejoin="round"
-									d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z"
+									d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
 								/>
 							</svg>
-							<div class="font-semibold">Range Events</div>
-						</div>
 
-						<!-- <div class="text-3xl mb-2">Out of range events</div> -->
-						<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 w-full gap-2">
-							{#each data.graphData as graphData}
-								{#if graphData.Value >= 120 || graphData.Value <= 70}
-									<div class="flex flex-col items-start bg-neutral-800 rounded">
-										<div class="">
-											<div class="text-red-500 font-semibold">
-												{graphData.Value} <span class="text-sm text-neutral-100">mg/dL</span>
-											</div>
-											<!-- {:else}  -->
-											<!-- <div class="text-emerald-500 font-semibold text-neutral-300">{graphData.Value}</div> -->
-											<!-- <div class="text-sm underline">mg/dL</div> -->
-										</div>
-										<div class="text-neutral-300 text-xs">{graphData.Timestamp}</div>
-									</div>
-								{/if}
-							{/each}
+							<div>Hide Alarms</div>
+						</div>
+					{:else}
+						<div class="flex gap-2 items-center">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								class="w-6 h-6"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+								/>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+								/>
+							</svg>
+
+							<div>Show Alarms</div>
+						</div>
+					{/if}
+				</button>
+
+				{#if showAlarms}
+					<div class="p-4 bg-neutral-800 rounded w-full">
+						<div class="text-xl md:text-2xl text-neutral-200">
+							<div class="flex gap-1 items-center">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									class="w-6 h-6"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+									/>
+								</svg>
+
+								<div class="font-semibold">Alarms</div>
+							</div>
+
+							<div class="flex gap-5 text-lg md:text-xl font-thin">
+								<div class="">High: {data.patient.data[0].targetHigh}</div>
+								<div class="">Low: {data.patient.data[0].targetLow}</div>
+							</div>
 						</div>
 					</div>
 				{/if}
-			</div>
 
-			<div class="w-full flex flex-col items-center justify-center gap-2 my-5">
 				<button
 					class="border border-teal-500 md:hover:border-teal-400 text-teal-500 md:hover:text-teal-400 transition-all duration-200 py-4 px-6 rounded w-full font-bold uppercase flex justify-center"
 					on:click={toggleEvents}
@@ -180,6 +217,52 @@
 						</div>
 					{/if}
 				</button>
+
+				<!-- ################################################### -->
+				<!-- OUT OF BOUNDS EVENTS -->
+				<!-- ################################################### -->
+				{#if showEvents}
+					<div class="p-4 bg-neutral-800 rounded w-full">
+						<div class="mt-5 text-lg md:text-2xl text-neutral-200">
+							<div class="flex gap-2 items-center">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									class="w-6 h-6"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z"
+									/>
+								</svg>
+								<div class="font-semibold">Range Events</div>
+							</div>
+
+							<!-- <div class="text-3xl mb-2">Out of range events</div> -->
+							<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 w-full gap-2">
+								{#each data.graphData as graphData}
+									{#if graphData.Value >= 120 || graphData.Value <= 70}
+										<div class="flex flex-col items-start bg-neutral-800 rounded">
+											<div class="">
+												<div class="text-red-500 font-semibold">
+													{graphData.Value} <span class="text-sm text-neutral-100">mg/dL</span>
+												</div>
+												<!-- {:else}  -->
+												<!-- <div class="text-emerald-500 font-semibold text-neutral-300">{graphData.Value}</div> -->
+												<!-- <div class="text-sm underline">mg/dL</div> -->
+											</div>
+											<div class="text-neutral-300 text-xs">{graphData.Timestamp}</div>
+										</div>
+									{/if}
+								{/each}
+							</div>
+						</div>
+					</div>
+				{/if}
 
 				<a
 					href="/"
